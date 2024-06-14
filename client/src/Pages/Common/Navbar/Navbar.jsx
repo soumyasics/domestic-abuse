b6df2a21b91bb0ac4807c76d41a28f8a11628198
Jithin Jose <jithinjose.sics@gmail.com>
2024-06-14 15:06:05 +0530

    minor change 3 sprint 1.1

--- client/src/Pages/Common/Navbar/Navbar.jsx
+++ client/src/Pages/Common/Navbar/Navbar.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 import brand from '../../../Assets/2a75e2f413ad511cbebf7abc265805b4.png';
 
 function Navbar() {
-    const navigate= useNavigate();
-    const handleImageClick = ()=>{
+    const navigate = useNavigate();
+
+    const handleImageClick = () => {
         navigate('/');
-    }
+    };
+
     return (
         <div className='container-fluid'>
-            <nav className="navbar navbar-expand-lg navbar-light" id='navbar-linked'>
-                <Link className="navbar-brand ms-5 me-0" to="/"><img src={brand} alt='brand' height={80} className='' /></Link>
-                <h1 className='navbar-brands d-flex align-self-end ms-0' onClick={handleImageClick}> <span className='theme-purple me-2'>Safe</span> <span className='theme-green'>Space</span></h1>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light" id='navbar-linked'>
+                <Link className="navbar-brand ms-5 me-0" to="/">
+                    <img src={brand} alt='brand' height={80} className='' />
+                </Link>
+                <h1 className='navbar-brands d-flex align-self-end ms-0' onClick={handleImageClick}>
+                    <span className='theme-purple me-2'>Safe</span>
+                    <span className='theme-green'>Space</span>
+                </h1>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse mx-5 justify-content-end" id="navbarNav">
@@ -28,20 +35,20 @@ function Navbar() {
                             <Link className="nav-link mx-3 theme-purple fw-semibold" to="/">Our Services</Link>
                         </li>
                         <li className="nav-item dropdown">
-                            <Link className="nav-link dropdown-toggle mx-3 theme-purple fw-semibold" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a className="nav-link dropdown-toggle mx-3 theme-purple fw-semibold" href="#" id="navbarDropdown1" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Login
-                            </Link>
-                            <div className="dropdown-menu dropdown-menu-end theme-purple" aria-labelledby="navbarDropdown">
-                                <Link className="dropdown-item theme-purple fw-semibold" to="/admin-login">Admin</Link>
-                                <Link className="dropdown-item theme-purple fw-semibold" to="#">Supporter</Link>
-                                <Link className="dropdown-item theme-purple fw-semibold" to="#">Legal Professional</Link>
-                            </div>
+                            </a>
+                            <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown1" >
+                                <li><Link className="dropdown-item theme-purple fw-semibold" to="/admin-login">Admin</Link></li>
+                                <li><Link className="dropdown-item theme-purple fw-semibold" to="#">Supporter</Link></li>
+                                <li><Link className="dropdown-item theme-purple fw-semibold" to="#">Legal Professional</Link></li>
+                            </ul>
                         </li>
                     </ul>
                 </div>
             </nav>
         </div>
-    )
+    );
 }
 
 export default Navbar;
