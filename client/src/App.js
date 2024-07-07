07adda4df3942fea4fc68b9c73b63cb15c3d0b8d
Jithin Jose <jithinjose.sics@gmail.com>
2024-07-07 15:31:01 +0530

    user home page added

--- client/src/App.js
+++ client/src/App.js
@@ -38,6 +38,7 @@ import AdminLegalProfessionalViewAll from './Pages/Admin/AdminLegalProfessionalV
 import UserLogin from './Pages/User/login/UserLogin';
 import UserRegister from './Pages/User/register/UserRegister';
 import UserForgotPassword from './Pages/User/UserForgotPassword/UserForgotPassword';
+import UserHome from './Pages/User/UserHome/UserHome';
 function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
@@ -50,6 +51,8 @@ function App() {
             <Route path='/user-login' element={[<Navbar/>,<UserLogin />]} />
             <Route path='/user-register' element={[<Navbar/>,<UserRegister />]} />
             <Route path='/user-forgot-password' element={[<Navbar/>,<UserForgotPassword/>]} />
+            <Route path='/user-home' element={[<Navbar/>,<UserHome/>]} />
+
 
             {/* Admin Paths */}
             <Route path='/admin-login' element={[<Navbar/>,<AdminLogin />]} />
